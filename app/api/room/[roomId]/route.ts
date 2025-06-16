cd3b7a371b0a34ab2e3ef3ab9cd99a730cb0b9fc
HuynhNgocXuan <131683766+HuynhNgocXuan@users.noreply.github.com>
2025-06-16 12:38:26 +0700

    fix: fix logic promise

--- app/api/room/[roomId]/route.ts
+++ app/api/room/[roomId]/route.ts
@@ -1,23 +1,23 @@
 import prismadb from "@/lib/prismadb";
 import { auth } from "@clerk/nextjs/server";
 import { NextResponse } from "next/server";
+import type { RouteContext } from "next"; // Optional
 
-export async function PATCH(
-  req: Request,
-  { params }: { params: { roomId: string } }
-) {
-  try { 
+export async function PATCH(req: Request, context: RouteContext) {
+  try {
     const body = await req.json();
     const { userId } = await auth();
 
     if (!userId) return new NextResponse("Unauthorized", { status: 401 });
 
-    if (!params.roomId)
+    const roomId = (context.params as { roomId: string }).roomId;
+
+    if (!roomId)
       return new NextResponse("Room ID is required", { status: 400 });
 
     const updatedRoom = await prismadb.room.update({
       where: {
-        id: params.roomId,
+        id: roomId,
       },
       data: {
         ...body,
@@ -31,18 +31,20 @@ export async function PATCH(
   }
 }
 
-export async function DELETE(req: Request, { params }: { params: { roomId: string } }) {
+export async function DELETE(req: Request, context: RouteContext) {
   try {
     const { userId } = await auth();
 
     if (!userId) return new NextResponse("Unauthorized", { status: 401 });
 
-    if (!params.roomId)
+    const roomId = (context.params as { roomId: string }).roomId;
+
+    if (!roomId)
       return new NextResponse("Room ID is required", { status: 400 });
 
     const deletedRoom = await prismadb.room.delete({
       where: {
-        id: params.roomId,
+        id: roomId,
       },
     });
 
--- app/page.tsx
+++ app/page.tsx
@@ -2,16 +2,17 @@ import { getHotels } from "@/actions/getHotels";
 import HotelList from "@/components/hotel/HotelList";
 
 interface HomeProps {
-  searchParams: {
+  searchParams: Promise<{
     title: string;
     country: string;
     state: string;
     city: string;
-  };
+  }>;
 }
 
+
 export default async function Home({ searchParams }: HomeProps) {
-  const hotels = await getHotels(searchParams);
+  const hotels = await getHotels(Promise.resolve(searchParams));
 
   return (
     <div>
